30f76453f3725cbb4fcc85b150789fa60af05895
kelsonbatista <kelsonbatista@gmail.com>
2022-12-28 19:18:40 -0300

    feat(tasks): added basic get and create methods

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,5 @@
 class HomeController < ApplicationController
   def welcome
-    @tasks = []
-    @tasks << Task.new('Buy milk')
-    @tasks << Task.new('Buy eggs')
-    @tasks << Task.new('Buy bread')
+    @tasks = Task.all
   end
 end
---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,10 @@
+class TasksController < ApplicationController
+  def new
+  end
+
+  def create
+    @task = Task.new(params.require(:task).permit(:description, :status))
+    @task.save
+    redirect_to root_path
+  end
+end 
