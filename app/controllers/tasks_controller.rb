0349bf552962687148411d06673ac6d8b2d7c327
kelsonbatista <kelsonbatista@gmail.com>
2022-12-28 23:44:06 -0300

    feat(tasks): added delete and show methods

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,40 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:edit, :update, :destroy, :show]
+
   def new
+
   end
 
   def create
-    @task = Task.new(params.require(:task).permit(:description, :status))
+    @task = Task.new(task_params)
     @task.save
     redirect_to root_path
   end
 
   def edit
-    @task = Task.find(params[:id])
+
   end
 
   def update
-    @task = Task.find(params[:id])
-    @task.update(params.require(:task).permit(:description, :status))
+    @task.update(task_params)
     redirect_to root_path
   end
-end 
+
+  def destroy
+    @task.destroy
+    redirect_to root_path
+  end
+
+  def show
+    
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:description, :status)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+end
